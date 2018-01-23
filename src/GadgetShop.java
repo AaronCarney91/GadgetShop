958a307a4a2611adc7432dace4f07fe20d9eb281
Aaron <aaron.carney1991@gmail.com>
2018-01-23 20:32:08 +0000

    Created program and added to github

---
+++ src/GadgetShop.java
@@ -0,0 +1,24 @@
+/**
+ * Created by u1264377 on 18/01/2018.
+ */
+public class GadgetShop {
+
+    private static final double WIDGET_PRICE = 1.69;
+    private static final double GADGET_PRICE = 2.12;
+    private static final double VAT_RATE = 0.175;
+    private static final double POSTAGE = 2.50;
+    private static final double WIDGET_SUP = 0.10;
+    private static final double GADGET_SUP = 0.08;
+    private double totalPostage = 0;
+    private double totalPrice = 0;
+    private double totalVat = 0;
+    private double totalWidgetPrice = 0;
+    private double totalGadgetPrice = 0;
+    private int widgets = 0;
+    private int gadgets = 0;
+
+    public GadgetShop()
+    {
+
+    }
+}
